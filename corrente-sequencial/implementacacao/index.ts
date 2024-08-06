73035bab30da9ccf836216082f77ee953d0bbd3c
vzsoares <vinicius.zenha@gmail.com>
2024-08-05 23:53:20 -0300

    feat: corrente-sequencial

--- corrente-sequencial/implementacacao/index.ts
+++ corrente-sequencial/implementacacao/index.ts
@@ -5,95 +5,120 @@ import {
     result4 as input4,
 } from "../base";
 
+type Result = { success: boolean; data: object; message?: string };
+
+interface Handler {
+    setNext(handler: Handler): Handler;
+    handle(request: unknown): Result;
+    hasNextHandler: boolean;
+}
+
 abstract class AbstractHandler implements Handler {
     private nextHandler: Handler | undefined;
+    public get hasNextHandler(): boolean {
+        return !!this.nextHandler;
+    }
+
     public setNext(handler: Handler): Handler {
         this.nextHandler = handler;
         return handler;
     }
-    public handle(request: unknown): null | ErrorData {
+    public handle(request: unknown) {
         if (this.nextHandler) {
             return this.nextHandler.handle(request);
         }
-        return null;
+        return { success: false, data: {}, message: "not processed" };
     }
 }
 
-class ZodErrorHandler extends AbstractHandler {
-    public handle(error: unknown): null | ErrorData {
-        if (error instanceof Error) {
-            if (error instanceof z.ZodError || error.name === "ZodError") {
-                const castedError = error as z.ZodError;
-                return {
-                    statusCode: 400,
-                    message: error.message,
-                    error: castedError.flatten(),
-                    code: "AC407E",
-                };
-            }
+class Base64Handler extends AbstractHandler {
+    public handle(data: unknown) {
+        try {
+            if (typeof data === "string") {
+                const r = atob(data);
+                return super.handle(r);
+            } else throw new Error("data not string");
+        } catch (error) {
+            return { success: false, data: {}, message: "invalid base64" };
         }
-        return super.handle(error);
     }
 }
 
-type Result = { success: boolean; data: object; message?: string };
-
-async function process(raw: unknown): Promise<Result> {
-    // check base64
-    let maybeBase64: string = "";
-    try {
-        if (typeof raw === "string") {
-            const r = atob(raw);
-            maybeBase64 = r;
+class JsonHandler extends AbstractHandler {
+    public handle(data: string) {
+        try {
+            const r = JSON.parse(data);
+            if (!r) return { success: false, data: {}, message: "no content" };
+            return super.handle(r);
+        } catch (error) {
+            return { success: false, data: {}, message: "invalid json" };
         }
-    } catch (error) {
-        return { success: false, data: {}, message: "invalid base64" };
     }
-    // check json
-    let maybeJson: Record<string, unknown> | undefined = undefined;
-    try {
-        if (typeof raw === "string") {
-            if (maybeBase64) {
-                const r = JSON.parse(maybeBase64);
-                maybeJson = r;
-            } else {
-                const r = JSON.parse(raw);
-                maybeJson = r;
-            }
-        }
-    } catch (error) {
-        return { success: false, data: {}, message: "invalid json" };
-    }
-    if (!maybeJson) return { success: false, data: {}, message: "no content" };
-    // check operation
-    const data = maybeJson;
-    if (!["UPDATE", "CREATE", "DELETE"].includes(`${data["operation"]}`)) {
-        return { success: false, data: {}, message: "invalid operation" };
-    }
-    // check source
-    if (!["mazden", "pinejot", "zordia"].includes(`${data["source"]}`)) {
-        return { success: false, data: {}, message: "source discontinued" };
-    }
+}
 
-    // check rules
-    if (data["operation"] === "UPDATE" && data["options"]?.["force"] === true) {
-        if (data["source"] === "pinejot") {
+class RootValidatorHandler extends AbstractHandler {
+    public handle(data: string) {
+        // check operation
+        if (!["UPDATE", "CREATE", "DELETE"].includes(`${data["operation"]}`)) {
             return {
                 success: false,
                 data: {},
-                message: "cannot force update for client pinejot",
+                message: "invalid operation",
             };
         }
+        // check source
+        if (!["mazden", "pinejot", "zordia"].includes(`${data["source"]}`)) {
+            return {
+                success: false,
+                data: {},
+                message: "source discontinued",
+            };
+        }
+        return super.handle(data);
     }
-    if (data["data"]?.["age"] > 21 && data["data"]?.["country"] === "BRA") {
-        return {
-            success: false,
-            data: {},
-            message: "forbiden age for brazil",
-        };
-    }
+}
 
-    return { success: true, data: data["data"] as object };
+class DomainRulesValidatorHandler extends AbstractHandler {
+    public handle(data: string) {
+        if (
+            data["operation"] === "UPDATE" &&
+            data["options"]?.["force"] === true
+        ) {
+            if (data["source"] === "pinejot") {
+                return {
+                    success: false,
+                    data: {},
+                    message: "cannot force update for client pinejot",
+                };
+            }
+        }
+        if (data["data"]?.["age"] > 21 && data["data"]?.["country"] === "BRA") {
+            return {
+                success: false,
+                data: {},
+                message: "forbiden age for brazil",
+            };
+        }
+
+        if (this.hasNextHandler) return super.handle(data);
+        else return { success: true, data: data["data"] as object };
+    }
+}
+
+const base64Handler = new Base64Handler();
+const jsonHandler = new JsonHandler();
+const rootValidatorHandler = new RootValidatorHandler();
+const domainRulesValidatorHandler = new DomainRulesValidatorHandler();
+
+base64Handler
+    .setNext(jsonHandler)
+    .setNext(rootValidatorHandler)
+    .setNext(domainRulesValidatorHandler);
+
+const requestHandler = base64Handler;
+
+async function process(raw: unknown): Promise<Result> {
+    return requestHandler.handle(raw);
 }
 
 process(input).then((r) => console.log(r));
